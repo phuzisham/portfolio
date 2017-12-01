fa17e7643335436f536f5fb3913b4a4f7cfd8c0b
David Hale <david.hale7@gmail.com>
2017-12-01 11:59:42 -0800

    projects migration

--- db/migrate/20171201191436_projects.rb
+++ db/migrate/20171201191436_projects.rb
@@ -1,4 +1,9 @@
 class Projects < ActiveRecord::Migration[5.1]
   def change
+    create_table :images do |t|
+      t.string 'title'
+      t.string 'description'
+      t.timestamps
+    end
   end
 end
