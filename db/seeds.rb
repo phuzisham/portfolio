87bd9fe3b9a55ab6f0605f31c0444a163e278279
David Hale <david.hale7@gmail.com>
2017-12-01 13:41:50 -0800

    seed sample project

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Project.destroy_all
+
+project_list = [
+  ["Sample Project", "Sample description...I figure, if I'm responsible for the end of the world, I get to name it. Who's calling me? Everybody I know lives here. I saw their production of 'Giselle' in 1890. I wept like a baby, and I was evil! Somebody put her tiny little thinking cap on! My Uncle Rory was the stodgiest taxidermist you've ever met by day. And you know the monkey's just,'I mock you with my monkey pants!' And what's with all the carrots? How did your brain even learn human speech? I'm just so curious. Shh! No programs, don't use that word. Just be."]
+]
+
+#seeding projects
+project_list.each do |title, description|
+  Project.create( title: title, description: description )
+end
+
+p "Created #{Project.count} project."
