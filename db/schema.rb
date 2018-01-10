ddbf16d89062bb5b00d53876bc9886a030e2df14
David Hale <david.hale7@gmail.com>
2018-01-09 21:17:51 -0800

    add link to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110022944) do
+ActiveRecord::Schema.define(version: 20180110051040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180110022944) do
     t.integer "photo_file_size"
     t.datetime "photo_updated_at"
     t.string "language"
+    t.string "link"
   end
 
   create_table "users", force: :cascade do |t|
