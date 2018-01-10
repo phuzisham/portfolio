b69db891e89c1e0021ee245fc625840446002baf
David Hale <david.hale7@gmail.com>
2018-01-09 21:10:17 -0800

    add language column to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171201232542) do
+ActiveRecord::Schema.define(version: 20180110022944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20171201232542) do
     t.string "photo_content_type"
     t.integer "photo_file_size"
     t.datetime "photo_updated_at"
+    t.string "language"
   end
 
   create_table "users", force: :cascade do |t|
