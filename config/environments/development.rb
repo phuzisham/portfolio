47294738afafb0a8b9c960ab5bc23000fab449d8
David Hale <david.hale7@gmail.com>
2018-01-14 15:21:15 -0800

    move paperclip s3 to production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,14 +36,6 @@ Rails.application.configure do
   }
   ActionMailer::Base.delivery_method = :smtp
 
-  config.paperclip_defaults = { :storage => :s3,
-    :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']},
-    :s3_host_name => 's3-us-west-2.amazonaws.com'}
-
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
