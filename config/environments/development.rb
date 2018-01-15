b154e0ec018ab524625844f9cfc28146955e3188
David Hale <david.hale7@gmail.com>
2018-01-14 16:13:33 -0800

    config mailgun settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,16 +26,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
-    :authentication => :plain,
-  }
-  ActionMailer::Base.delivery_method = :smtp
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
