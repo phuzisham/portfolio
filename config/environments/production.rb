c587fcf3394aa391fe85d066b8f19f96a2edc835
David Hale <david.hale7@gmail.com>
2018-01-15 10:36:46 -0800

    change back to mailgun smtp vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,10 +2,10 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   ActionMailer::Base.smtp_settings = {
-    :port           => 587,
-    :address        => 'smtp.gmail.com',
-    :user_name      => ENV['GMAIL_NAME'],
-    :password       => ENV['GMAIL_PASS'],
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain         => 'david-hale-portfolio.herokuapp.com',
     :authentication => :plain,
   }
